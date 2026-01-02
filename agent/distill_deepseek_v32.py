3108deb5e0301255b51bd3345be52a99539108eb
Tianang Leng <lingchunsusan@gmail.com>
2026-01-02 01:24:08 -0500

    regular update

--- agent/distill_deepseek_v32.py
+++ agent/distill_deepseek_v32.py
@@ -33,8 +33,8 @@ API_KEY = "EMPTY"
 MAX_SAMPLES = 3
 BATCH_SIZE = 128  # Rows per task chunk
 OUTPUT_BASE_DIR = Project_root / "DataPrepare/SFT_data/DeepSeek_V32_distill_agent_data"
-ALPACA_DIR = OUTPUT_BASE_DIR / "intern-s1-mini_TDC_train_Alpaca_per_task"
-RAW_NORMALIZED_DIR = OUTPUT_BASE_DIR / "intern-s1-mini_TDC_train_raw_normalized_messages_per_task"
+ALPACA_DIR = OUTPUT_BASE_DIR / "intern-s1-mini_TDC_train_Alpaca_per_task"  # 注意 Alpaca 格式需要自己指定，这份文件里面已经指定适应了 intern-s1 的格式
+RAW_NORMALIZED_DIR = OUTPUT_BASE_DIR / "TDC_train_raw_normalized_messages_per_task"
 STATE_FILE = Current_dir / "distill_state.json"
 DATA_SOURCE_DIR = Project_root / "DataPrepare/TDC_train_prompts_label"
 
