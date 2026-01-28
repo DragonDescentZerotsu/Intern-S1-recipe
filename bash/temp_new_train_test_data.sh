fb77ecdf0b048dc31c29e57b182c69229cbfac14
Tianang Leng <lingchunsusan@gmail.com>
2026-01-27 22:36:57 -0500

    node002 regular update

--- bash/temp_new_train_test_data.sh
+++ bash/temp_new_train_test_data.sh
@@ -1,5 +1,5 @@
 rm -rf DataPrepare/TDC_train_prompts_label_scaffold/*
 rm -rf DataPrepare/TDC_test_prompts_label_scaffold/*
 
-# python DataPrepare/process_tdc_train_test_split.py --target-split train
-# python DataPrepare/process_tdc_train_test_split.py --target-split test
+python DataPrepare/process_tdc_train_test_split.py --target-split train
+python DataPrepare/process_tdc_train_test_split.py --target-split test
